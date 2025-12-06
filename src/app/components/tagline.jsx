54855d964f3ce6994f95d44f6317c15fffa9cffe
rukmanabali87 <herakkes04@gmail.com>
2025-12-06 14:55:13 +0800

    update tagline

--- src/app/components/tagline.jsx
+++ src/app/components/tagline.jsx
@@ -10,27 +10,26 @@ export default function Tagline(){
                 <div className="grid grid-cols-1">
                     <div className="flex items-center justify-between">
                         <ul className="list-none space-x-2">
-                            <li className="inline-flex items-center">
+                            {/* <li className="inline-flex items-center">
                                 <FiClock className="text-primary size-4"></FiClock>
                                 <span className="ms-2 text-slate-300">Mon-Sat: 9am to 6pm</span>
                             </li>
                             <li className="inline-flex items-center ms-2">
                                 <FiMapPin className="text-primary size-4"></FiMapPin>
                                 <span className="ms-2 text-slate-300">Houston, USA 485</span>
-                            </li>
+                            </li> */}
                         </ul>
 
                         <ul className="list-none">
                             <li className="inline-flex items-center">
                                 <FiMail className="text-primary size-4"></FiMail>
-                                <a href="mailto:contact@example.com" className="ms-2 text-slate-300 hover:text-slate-200">contact@example.com</a>
+                                <a href="mailto:info@rukmanabalitour.com" className="ms-2 text-slate-300 hover:text-slate-200">info@rukmanabalitour.com</a>
                             </li>
                             <li className="inline-flex items-center ms-2">
                                 <ul className="list-none space-x-3">
-                                    <li className="inline-flex mb-0"><a href="#!" className="text-slate-300 hover:text-primary"><FiFacebook className="size-4 align-middle" title="facebook"></FiFacebook></a></li>
-                                    <li className="inline-flex ms-2 mb-0"><a href="#!" className="text-slate-300 hover:text-primary"><FiInstagram className="size-4 align-middle" title="instagram"></FiInstagram></a></li>
-                                    <li className="inline-flex ms-2 mb-0"><a href="#!" className="text-slate-300 hover:text-primary"><FiTwitter className="size-4 align-middle" title="twitter"></FiTwitter></a></li>
-                                    <li className="inline-flex ms-2 mb-0"><a href="tel:+152534-468-854" className="text-slate-300 hover:text-primary"><FiPhone className="size-4 align-middle" title="phone"></FiPhone></a></li>
+                                    <li className="inline-flex mb-0"><a href="https://www.facebook.com/rukmanabalitour" className="text-slate-300 hover:text-primary"><FiFacebook className="size-4 align-middle" title="facebook"></FiFacebook></a></li>
+                                    <li className="inline-flex ms-2 mb-0"><a href="https://www.instagram.com/rukmanabalitour/" className="text-slate-300 hover:text-primary"><FiInstagram className="size-4 align-middle" title="instagram"></FiInstagram></a></li>
+                                    <li className="inline-flex ms-2 mb-0"><a href="https://wa.me/6287862207901" className="text-slate-300 hover:text-primary"><FiPhone className="size-4 align-middle" title="phone"></FiPhone></a></li>
                                 </ul>
                             </li>
                         </ul>
