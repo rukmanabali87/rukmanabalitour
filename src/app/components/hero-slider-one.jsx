22df5e28f45af38d55cc3d256b33773b1424f6ac
rukmanabali87 <herakkes04@gmail.com>
2025-12-07 14:15:32 +0800

    update warna tagline, navbar, text navbar, hover navbar, hover tagline

--- src/app/components/hero-slider-one.jsx
+++ src/app/components/hero-slider-one.jsx
@@ -12,12 +12,12 @@ export default function HeroSliderOne(){
             <div className="swiper-container absolute end-0 top-0 w-full h-full">
                 <Swiper className="swiper-wrapper" loop={true} speed={2000} autoplay={{delay:6500}} effect="flip" modules={[Autoplay,EffectFade ]}>
                     <SwiperSlide className="swiper-slide flex items-center overflow-hidden">
-                        <div className="slide-inner absolute end-0 top-0 w-full h-full slide-bg-image flex items-center bg-center" style={{backgroundImage:`url('/images/bg/2.jpg')`}}>
-                            <div className="absolute inset-0 bg-slate-900/70"></div>
+                        <div className="slide-inner absolute end-0 top-0 w-full h-full slide-bg-image flex items-center bg-center" style={{backgroundImage:`url('/images/bg/8.jpg')`}}>
+                            <div className="absolute inset-0 bg-slate-900/30"></div>
                             <div className="container relative">
                                 <div className="grid grid-cols-1">
                                     <div className="text-center">
-                                        <Image src='/images/map-plane.png' width={0} height={0} sizes='100vw' style={{width:'300px', height:'auto'}} className="mx-auto w-[300px]" alt=""/>
+                                        {/* <Image src='/images/map-plane.png' width={0} height={0} sizes='100vw' style={{width:'300px', height:'auto'}} className="mx-auto w-[300px]" alt=""/> */}
                                         <h1 className="font-bold text-white lg:leading-normal leading-normal text-4xl lg:text-6xl mb-6 mt-5">Find Your Best <br/> Travels Package</h1>
                                         <p className="text-white/70 text-xl max-w-xl mx-auto">Planning for a trip? We will organize your trip with the best places and within best budget!</p>
                                         
@@ -36,7 +36,7 @@ export default function HeroSliderOne(){
                             <div className="container relative">
                                 <div className="grid grid-cols-1">
                                     <div className="text-center">
-                                        <img src='/images/map-plane.png' width={0} height={0} sizes='100vw' style={{width:'300px', height:'auto'}} className="mx-auto w-[300px]" alt=""/>
+                                        {/* <img src='/images/map-plane.png' width={0} height={0} sizes='100vw' style={{width:'300px', height:'auto'}} className="mx-auto w-[300px]" alt=""/> */}
                                         <h1 className="font-bold text-white lg:leading-normal leading-normal text-4xl lg:text-6xl mb-6 mt-5">Find Next Best <br/> Place To Visit</h1>
                                         <p className="text-white/70 text-xl max-w-xl mx-auto">Planning for a trip? We will organize your trip with the best places and within best budget!</p>
                                         
--- src/app/components/tagline.jsx
+++ src/app/components/tagline.jsx
@@ -5,7 +5,7 @@ import {FiClock, FiMapPin, FiMail, FiFacebook, FiInstagram, FiTwitter, FiPhone} 
 export default function Tagline(){
     return(
         <>
-        <div className="tagline bg-slate-900">
+        <div className="tagline bg-[#0B472D]">
             <div className="container relative">                
                 <div className="grid grid-cols-1">
                     <div className="flex items-center justify-between">
@@ -22,14 +22,14 @@ export default function Tagline(){
 
                         <ul className="list-none">
                             <li className="inline-flex items-center">
-                                <FiMail className="text-primary size-4"></FiMail>
+                                <FiMail className="text-slate-300 size-4"></FiMail>
                                 <a href="mailto:info@rukmanabalitour.com" className="ms-2 text-slate-300 hover:text-slate-200">info@rukmanabalitour.com</a>
                             </li>
                             <li className="inline-flex items-center ms-2">
                                 <ul className="list-none space-x-3">
-                                    <li className="inline-flex mb-0"><a href="https://www.facebook.com/rukmanabalitour" className="text-slate-300 hover:text-primary"><FiFacebook className="size-4 align-middle" title="facebook"></FiFacebook></a></li>
-                                    <li className="inline-flex ms-2 mb-0"><a href="https://www.instagram.com/rukmanabalitour/" className="text-slate-300 hover:text-primary"><FiInstagram className="size-4 align-middle" title="instagram"></FiInstagram></a></li>
-                                    <li className="inline-flex ms-2 mb-0"><a href="https://wa.me/6287862207901" className="text-slate-300 hover:text-primary"><FiPhone className="size-4 align-middle" title="phone"></FiPhone></a></li>
+                                    <li className="inline-flex mb-0"><a href="https://www.facebook.com/rukmanabalitour" className="text-slate-300 hover:text-[#DEA243]"><FiFacebook className="size-4 align-middle" title="facebook"></FiFacebook></a></li>
+                                    <li className="inline-flex ms-2 mb-0"><a href="https://www.instagram.com/rukmanabalitour/" className="text-slate-300 hover:text-[#DEA243]"><FiInstagram className="size-4 align-middle" title="instagram"></FiInstagram></a></li>
+                                    <li className="inline-flex ms-2 mb-0"><a href="https://wa.me/6287862207901" className="text-slate-300 hover:text-[#DEA243]"><FiPhone className="size-4 align-middle" title="phone"></FiPhone></a></li>
                                 </ul>
                             </li>
                         </ul>
